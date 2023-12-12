1e3e0294bb68d62ea5f4552047a2383837ef9006
Ruben Carvalho <rucarva@amazon.com>
2023-12-12 18:18:53 +0000

    chore: Clean up git cached

--- demo/build/static/assets/copyable-9846e1d4.js
+++
@@ -1,5 +0,0 @@
-import{j as o,H as e,C as n,a as c}from"./index-86469c5d.js";const t=`cd project
-echo 'hello' > demo.txt
-git add .
-git commit
-`;function s(){return o.jsxs(o.Fragment,{children:[o.jsx(e,{description:"Code snippet with a copy to clipboard button",children:"Copyable demo"}),o.jsx(n,{content:t,copyButton:o.jsx(c,{buttonAriaLabel:"Copy code",successText:"Snippet copied",errorText:"Could not copy the snippet",content:t})})]})}export{s as default};
--- demo/build/static/assets/styles.module-48003790.js
+++
@@ -1,1 +0,0 @@
-const l="_scrollable_ko2oz_1",o="_limitedWidth_ko2oz_6",s={scrollable:l,limitedWidth:o};export{s as c};
