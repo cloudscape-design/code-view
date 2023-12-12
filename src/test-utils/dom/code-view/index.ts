b4dc7a2a0ce0211848dd773147bdce27701839bc
Ruben Carvalho <rucarva@amazon.com>
2023-12-12 17:16:11 +0000

    chore: Add test utils

--- src/test-utils/dom/code-view/index.ts
+++ src/test-utils/dom/code-view/index.ts
@@ -1,12 +1,24 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 // import componentsWrapper from "@cloudscape-design/components/test-utils/dom";
-import { ComponentWrapper } from "@cloudscape-design/test-utils-core/dom";
+import { ComponentWrapper, ElementWrapper } from "@cloudscape-design/test-utils-core/dom";
 import * as styles from "../../../code-view/styles.css";
 
 export default class CodeViewWrapper extends ComponentWrapper {
   static rootSelector: string = styles.root;
   findCodeView(): ComponentWrapper {
     return this.findByClassName(CodeViewWrapper.rootSelector)!;
+  }
+
+  findCodeContent(): ElementWrapper {
+    return this.findByClassName(styles.code)!;
+  }
+
+  findCopyButtonSlot(): ElementWrapper {
+    return this.findByClassName(styles.copyButton)!;
+  }
+
+  findLineNumbers(): ElementWrapper {
+    return this.findByClassName(styles.lineNumbers)!;
   }
 }
