0b79a0ce74391af9431f0cc8b282924877683a74
Ruben Carvalho <rucarva@amazon.com>
2023-12-12 21:43:48 +0000

    chore: Clean up

--- vite.unit.config.js
+++ vite.unit.config.js
@@ -1,6 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import process from "node:process";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vite";
 import base from "./vite.config.js";
