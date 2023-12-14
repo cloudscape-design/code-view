dcbd4f2b52add0dfe21b1aa53e46fca5f4538edf
Ruben Carvalho <rucarva@amazon.com>
2023-12-14 09:41:02 +0000

    chore: Update Contributing

--- build/demo/snippets/simple.tsx
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-import CodeView from "@cloudscape-design/code-view/code-view";
-
-export default function () {
-  return <CodeView content="your content" />;
-}
