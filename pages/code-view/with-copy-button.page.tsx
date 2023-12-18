2f78dcd2887ebe2dfbcec3ed7c4525a8fba98a50
Ruben Carvalho <rucarva@amazon.de>
2023-12-18 16:20:49 +0100

    chore: Remove Box and fix styling and highlighting imports

--- pages/code-view/with-copy-button.page.tsx
+++ pages/code-view/with-copy-button.page.tsx
@@ -1,22 +1,18 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Box } from "@cloudscape-design/components";
 import { Button } from "@cloudscape-design/components";
 import { CodeView } from "../../lib/components";
-// import jsHighlight from "../../lib/components/code-view/highlight/javascript";
 import { ScreenshotArea } from "../screenshot-area";
 export default function CodeViewPage() {
   return (
     <ScreenshotArea>
       <h1>Code View</h1>
       <main>
-        <Box>
-          <CodeView
-            copyButton={<Button>Copy</Button>}
-            content={`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`}
-          />
-        </Box>
+        <CodeView
+          copyButton={<Button iconName="copy"></Button>}
+          content={`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`}
+        />
       </main>
     </ScreenshotArea>
   );
--- pages/code-view/with-line-numbers.page.tsx
+++ pages/code-view/with-line-numbers.page.tsx
@@ -1,24 +1,20 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Box } from "@cloudscape-design/components";
 import { CodeView } from "../../lib/components";
 
-// import jsHighlight from "../../lib/components/code-view/highlight/javascript";
 import { ScreenshotArea } from "../screenshot-area";
 export default function CodeViewPage() {
   return (
     <ScreenshotArea>
       <h1>Code View</h1>
       <main>
-        <Box>
-          <CodeView
-            content={`Hello
+        <CodeView
+          content={`Hello
           World
           !`}
-            lineNumbers={true}
-          />
-        </Box>
+          lineNumbers={true}
+        />
       </main>
     </ScreenshotArea>
   );
