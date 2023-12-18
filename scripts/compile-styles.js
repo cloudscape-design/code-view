9734de49ad2d87c00f42b243730154ba8e75d1b9
Ruben Carvalho <rucarva@amazon.de>
2023-12-18 16:11:05 +0100

    chore: Add comments to compile-styles script

--- scripts/compile-styles.js
+++ scripts/compile-styles.js
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 import { ThemeBuilder, buildThemedComponentsInternal } from "@cloudscape-design/theming-build";
 
+// Generates scoped styles for the component
+// The output includes files like 'styles.css.js', 'styles.scoped.css', and 'styles.selectors.js'
 await buildThemedComponentsInternal({
-  primary: new ThemeBuilder("unused", ":root", []).build(),
-  componentsOutputDir: "lib/components",
-  skip: ["design-tokens", "preset"],
-  scssDir: "src",
-  variablesMap: {},
-  exposed: [],
+  primary: new ThemeBuilder("unused", ":root", []).build(), // "unused" as placeholder and ":root" for global style
+  componentsOutputDir: "lib/components", // sets the output directory for the processed styles
+  skip: ["design-tokens", "preset"], // skips generation of design tokens and presets
+  scssDir: "src", // sets the source directory for SCSS files
+  variablesMap: {}, // not needed - can be used for custom token-variable mappings
+  exposed: [], // not needed - can be used for exposing specific design tokens
 });
