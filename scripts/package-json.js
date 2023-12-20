561637fe7d5c524945cb7615d5e6cd4ae2d89e07
Rúben Carvalho <rucarva@amazon.de>
2023-12-20 12:24:45 +0100

    feat: Add scripts (#9)
    * feat: Add scripts

---
+++ scripts/package-json.js
@@ -0,0 +1,16 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import fs from "node:fs";
+import { writeJSON } from "./utils.js";
+
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
+
+mainPackage();
+
+function mainPackage() {
+  writeJSON("lib/components/package.json", {
+    ...pkg,
+    // prevent postinstall scripts we use in our build from being published
+    scripts: undefined,
+  });
+}
