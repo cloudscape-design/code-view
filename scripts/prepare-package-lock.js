2548d5aa8b65aa82e526f72bdfdf2de3e376b0ef
Ruben Carvalho <rucarva@amazon.de>
2024-01-11 16:16:12 +0100

    chore: Update scripts

--- scripts/prepare-package-lock.js
+++ scripts/prepare-package-lock.js
@@ -13,28 +13,20 @@ const require = createRequire(import.meta.url);
 const filename = require.resolve("../package-lock.json");
 const packageLock = require(filename);
 
-if (packageLock.lockfileVersion !== 2) {
-  throw new Error('package-lock.json must have "lockfileVersion": 2');
+if (packageLock.lockfileVersion !== 3) {
+  throw Error("package-lock.json file is not version 3. Use regular npm to update the packages.");
 }
 
-function unlock(packages) {
-  Object.keys(packages).forEach((dependencyName) => {
-    const dependency = packages[dependencyName];
-
-    if (dependencyName.includes("@cloudscape-design/")) {
-      delete packages[dependencyName];
-    } else if (dependency.resolved && dependency.resolved.includes("codeartifact.us-west-2.amazonaws.com")) {
-      throw Error(
-        "package-lock.json file contains a reference to CodeArtifact. Use regular npm to update the packages."
-      );
-    }
-  });
-
-  return packages;
-}
-
-packageLock.packages = unlock(packageLock.packages);
-packageLock.dependencies = unlock(packageLock.dependencies);
+Object.keys(packageLock.packages).forEach((dependencyName) => {
+  const dependency = packageLock.packages[dependencyName];
+  if (dependencyName.includes("@cloudscape-design/")) {
+    delete packageLock.packages[dependencyName];
+  } else if (dependency.resolved && dependency.resolved.indexOf("codeartifact.us-west-2.amazonaws.com") !== -1) {
+    throw Error(
+      `package-lock.json file contains a reference to CodeArtifact at ${dependencyName}. Use regular npm to update the packages.`
+    );
+  }
+});
 
 fs.writeFileSync(filename, JSON.stringify(packageLock, null, 2) + "\n");
 console.log("Removed @cloudscape-design/ dependencies from package-lock file");
