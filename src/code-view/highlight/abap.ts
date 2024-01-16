ea3f443865381f38b3e2638077e0692540afc02a
Ruben Carvalho <rucarva@amazon.de>
2024-01-16 10:00:49 +0100

    chore: Remove ABAP language

--- src/code-view/highlight/abap.ts
+++
@@ -1,6 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-import { AbapHighlightRules } from "ace-code/src/mode/abap_highlight_rules";
-import { createHighlight } from ".";
-
-export default createHighlight(new AbapHighlightRules());
