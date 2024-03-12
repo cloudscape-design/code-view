4cd5711d9852368d630a38fbf6df8ca622bee493
Ruben Carvalho <rucarva@amazon.de>
2024-03-12 12:48:24 +0100

    fix: Fix code indentation by setting white-space to pre

--- pages/code-view/with-line-wrapping.page.tsx
+++ pages/code-view/with-line-wrapping.page.tsx
@@ -29,6 +29,11 @@ export default function CodeViewPage() {
           lineNumbers={true}
           content={`Hello this is a short line.\nHello this is a long line. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\nHello this is another short line.`}
         />
+        <CodeView
+          lineWrapping={true}
+          lineNumbers={true}
+          content={`public class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println("Hello, World!");\n    }\n}`}
+        />
       </SpaceBetween>
     </ScreenshotArea>
   );
