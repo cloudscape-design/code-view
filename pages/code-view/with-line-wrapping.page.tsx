b0659b68daff562846cda995de119961a97591ae
Alice Koreman <alicekor@amazon.nl>
2024-12-03 00:07:34 +0100

    add syntax highlighting example line wrapping page

--- pages/code-view/with-line-wrapping.page.tsx
+++ pages/code-view/with-line-wrapping.page.tsx
@@ -4,7 +4,9 @@
 import { SpaceBetween } from "@cloudscape-design/components";
 
 import { CodeView } from "../../lib/components";
+import cppHighlight from "../../lib/components/code-view/highlight/cpp";
 import { ScreenshotArea } from "../screenshot-area";
+
 export default function CodeViewPage() {
   return (
     <ScreenshotArea>
@@ -30,10 +32,12 @@ export default function CodeViewPage() {
           lineNumbers={true}
           content={`Hello this is a short line.\nHello this is a long line. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\nHello this is another short line.`}
         />
+        Full example with indentation and code highlighting
         <CodeView
           lineWrapping={true}
           lineNumbers={true}
-          content={`public class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println("Hello, World!");\n    }\n}`}
+          highlight={cppHighlight}
+          content={`void UserQuery(map<string, vector<string> > &svmap) {\n    string queryName;\n    cout << "Please enter a family name you want to query: ";\n    cin >> queryName;\n    int i = 0;\n    for (map<string, vector<string> >::iterator itr = svmap.begin(), mapEnd = svmap.end(); itr != mapEnd; ++itr) {\n        i++;\n        if (itr->first == queryName) {\n            cout << "The " << itr->first << " family has " << itr->second.size() << " children: ";\n            for (vector<string>::iterator itrvec = itr->second.begin(), vecEnd = itr->second.end(); itrvec != vecEnd; ++itrvec)\n                cout << *itrvec << " ";\n            break;\n        }\n    }\n    if (i >= svmap.size())\n        cout << "Sorry, the " << queryName << " family is not found." << endl;\n}`}
         />
       </SpaceBetween>
     </ScreenshotArea>
