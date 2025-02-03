4b59bb3919efc281de4a6b15566e78b15b9131da
Avinash Dwarapu <dwaraa@amazon.com>
2025-02-03 10:04:32 +0100

    fix: Fix line numbers incorrectly wrapping inside `word-break: break-word` parents

--- pages/code-view/with-line-numbers.page.tsx
+++ pages/code-view/with-line-numbers.page.tsx
@@ -17,6 +17,13 @@ export default function CodeViewPage() {
           content={`# Hello World`}
           actions={<Button ariaLabel="Copy code" iconName="copy"></Button>}
         />
+        {/* Wrapping should not be affected by the parent's word-break property. */}
+        <div style={{ wordBreak: "break-word" }}>
+          <CodeView
+            lineNumbers={true}
+            content={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => `This is line number #${i + 1}.`).join("\n")}
+          />
+        </div>
       </SpaceBetween>
     </ScreenshotArea>
   );
