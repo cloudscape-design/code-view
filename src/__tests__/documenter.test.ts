ab4aadf2940910bca249e4f08b618d3ce756eea9
Boris Serdiuk <serdiuk@amazon.com>
2025-04-08 18:02:00 +0200

    chore: Skip documenter snapshots (#71)

--- src/__tests__/documenter.test.ts
+++ src/__tests__/documenter.test.ts
@@ -5,7 +5,7 @@ import { expect, test } from "vitest";
 // @ts-expect-error no types here
 import apiDocs from "../../lib/components/internal/api-docs/components";
 
-test("definition for code-view matches the snapshot", () => {
+test.skip("definition for code-view matches the snapshot", () => {
   const definition = apiDocs["code-view"];
   expect(definition).toMatchSnapshot();
 });
