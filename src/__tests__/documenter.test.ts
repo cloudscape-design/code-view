9c7f0c3fbe886735e9253af8549f244224574465
Amr Mohamed <taheramr@amazon.de>
2025-05-22 13:20:02 +0200

    chore: Add documenter tests

--- src/__tests__/documenter.test.ts
+++ src/__tests__/documenter.test.ts
@@ -2,8 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 import { expect, test } from "vitest";
 
-import apiDocs from "../../lib/components/internal/api-docs/components";
+import componentDefinitions from "../../lib/components/internal/api-docs/components";
+import { getAllComponents } from "./utils";
 
-test.each(Object.entries(apiDocs))("definition for $0 matches the snapshot", (_name, definition) => {
-  expect(definition).toMatchSnapshot();
+test.each<string>(getAllComponents())(`definition for %s matches the snapshot`, (componentName: string) => {
+  const definition = componentDefinitions[componentName];
+  // overriding with a fake value so that when there are icon changes in components this test doesn't block it
+  const iconNameDefinition = definition.properties.find(({ name }: { name: string }) => name === "iconName");
+  if (iconNameDefinition && iconNameDefinition.inlineType?.type === "union") {
+    iconNameDefinition.inlineType.values = ["comes from @cloudscape-design/components"];
+  }
+  expect(definition).toMatchSnapshot(componentName);
 });
