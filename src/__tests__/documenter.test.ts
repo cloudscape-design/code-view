8c840b8d5acea2fcb6a1348e95bf410930e2ddcc
Boris Serdiuk <serdiuk@amazon.com>
2025-06-13 11:34:38 +0200

    chore: Migrate to the new documenter API (#87)

--- src/__tests__/documenter.test.ts
+++ src/__tests__/documenter.test.ts
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 import { expect, test } from "vitest";
 
-// @ts-expect-error no types here
 import apiDocs from "../../lib/components/internal/api-docs/components";
 
 test.each(Object.entries(apiDocs))("definition for $0 matches the snapshot", (name, definition) => {
